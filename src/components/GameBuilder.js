a8ee4a7d96fa6e64341561d7a1864aee30c79784
Justin Elue <mjuelue@gmail.com>
2023-07-13 13:16:27 -0500

    routes fixed

--- src/components/GameBuilder.js
+++ src/components/GameBuilder.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function GameBuilder() {
-
+return (
+    <div className="StoryPage min-h-screen bg-[#77867F] w-screen flex flex-col justify-center snap-start">
+        <p>TESTESTEST</p>
+    </div>
+)
 }
 
 export default GameBuilder;
